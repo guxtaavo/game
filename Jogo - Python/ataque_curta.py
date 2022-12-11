6328b8edba61e555ba3cc2993746aeb22f538f69
Gustavo Nunes <109112700+guxtaavo@users.noreply.github.com>
2022-12-11 14:17:06 -0300

    ataque curto piscando

--- Jogo - Python/ataque_curta.py
+++ Jogo - Python/ataque_curta.py
@@ -21,13 +21,12 @@ class Curta_Distancia:
             if pg.key.get_pressed()[pg.K_e]:
                 self.distanciap1p2 = math.sqrt(math.pow((ConfiJogo.P1_POSICAO_X - ConfiJogo.P2_POSICAO_X), 2)+math.pow((ConfiJogo.P1_POSICAO_Y - ConfiJogo.P2_POSICAO_Y), 2))
                 self.distanciap1minion = math.sqrt(math.pow((ConfiJogo.P1_POSICAO_X - ConfiJogo.MINION_POSICAO_X), 2)+math.pow((ConfiJogo.P1_POSICAO_Y - ConfiJogo.MINION_POSICAO_Y), 2))
+                pg.draw.rect(self.tela, ConfiJogo.PRETO, (ConfiJogo.P1_POSICAO_X, ConfiJogo.P1_POSICAO_Y, ConfiJogo.LARGURA_P1, ConfiJogo.ALTURA_P1))
                 if self.distanciap1p2 < 60:
                     ConfiJogo.VIDA_P2 -= self.dano
-                    if ConfiJogo.ULTIMO_PASSO_P1 == "NORTE" or ConfiJogo.ULTIMO_PASSO_P1 == "SUL" or ConfiJogo.ULTIMO_PASSO_P1 == "LESTE" or ConfiJogo.ULTIMO_PASSO_P1 == "OESTE":
-                        pg.draw.rect(self.tela, ConfiJogo.PRETO, (ConfiJogo.P1_POSICAO_X, ConfiJogo.P1_POSICAO_Y, ConfiJogo.LARGURA_P1, ConfiJogo.ALTURA_P1))
                 elif self.distanciap1minion < 60:
                     ConfiJogo.VIDA_MINION -= self.dano
-                    pg.draw.rect(self.tela, ConfiJogo.PRETO, (ConfiJogo.P1_POSICAO_X, ConfiJogo.P1_POSICAO_Y, ConfiJogo.LARGURA_P1, ConfiJogo.ALTURA_P1))
+                    
 
 
         def ataque_p2(self, tela):  #alvo #distancia 2p = sqrt((x-xo)^2+(y-yo)^2)
@@ -35,27 +34,21 @@ class Curta_Distancia:
             if pg.key.get_pressed()[pg.K_p]:
                 self.distanciap2p1 = math.sqrt(math.pow((ConfiJogo.P2_POSICAO_X - ConfiJogo.P1_POSICAO_X), 2)+math.pow((ConfiJogo.P2_POSICAO_Y - ConfiJogo.P1_POSICAO_Y), 2))
                 self.distanciap2minion = math.sqrt(math.pow((ConfiJogo.P2_POSICAO_X - ConfiJogo.MINION_POSICAO_X), 2)+math.pow((ConfiJogo.P2_POSICAO_Y - ConfiJogo.MINION_POSICAO_Y), 2))
+                pg.draw.rect(self.tela, ConfiJogo.PRETO, (ConfiJogo.P2_POSICAO_X, ConfiJogo.P2_POSICAO_Y, ConfiJogo.LARGURA_P2, ConfiJogo.ALTURA_P2))
                 if self.distanciap2p1 < 60:
                     ConfiJogo.VIDA_P1 -= self.dano
-                    if ConfiJogo.ULTIMO_PASSO_P2 == "NORTE" or ConfiJogo.ULTIMO_PASSO_P2 == "SUL" or ConfiJogo.ULTIMO_PASSO_P2 == "LESTE" or ConfiJogo.ULTIMO_PASSO_P2 == "OESTE":
-                        pg.draw.rect(self.tela, ConfiJogo.PRETO, (ConfiJogo.P2_POSICAO_X, ConfiJogo.P2_POSICAO_Y, ConfiJogo.LARGURA_P2, ConfiJogo.ALTURA_P2))
                 elif self.distanciap2minion < 60:
                     ConfiJogo.VIDA_MINION -= self.dano
-                    pg.draw.rect(self.tela, ConfiJogo.PRETO, (ConfiJogo.P2_POSICAO_X, ConfiJogo.P2_POSICAO_Y, ConfiJogo.LARGURA_P2, ConfiJogo.ALTURA_P2))
-
 
 
         def ataque_p3(self, tela):  #alvo #distancia 2p = sqrt((x-xo)^2+(y-yo)^2)
             self.tela = tela
-           
             self.distanciap3p1 = math.sqrt(math.pow((ConfiJogo.MINION_POSICAO_X - ConfiJogo.P1_POSICAO_X), 2)+math.pow((ConfiJogo.MINION_POSICAO_Y - ConfiJogo.P1_POSICAO_Y), 2))
-            
             self.distanciap3p2= math.sqrt(math.pow((ConfiJogo.MINION_POSICAO_X - ConfiJogo.P2_POSICAO_X), 2)+math.pow((ConfiJogo.MINION_POSICAO_Y - ConfiJogo.P2_POSICAO_Y), 2))
                 
             if self.distanciap3p1 < 60:
                 ConfiJogo.VIDA_P1 -= self.dano
                 pg.draw.rect(self.tela, ConfiJogo.PRETO, (ConfiJogo.MINION_POSICAO_X, ConfiJogo.MINION_POSICAO_Y, ConfiJogo.LARGURA_P3, ConfiJogo.ALTURA_P3))
-
             elif self.distanciap3p2 < 60:
                 ConfiJogo.VIDA_P2 -= self.dano
                 pg.draw.rect(self.tela, ConfiJogo.PRETO, (ConfiJogo.MINION_POSICAO_X, ConfiJogo.MINION_POSICAO_Y, ConfiJogo.LARGURA_P3, ConfiJogo.ALTURA_P3))
